e71c957d95a2a744277c8f4e2168658eb9fad746
Neal Bayya <nealbayya@petrosian.local>
2018-06-13 00:15:31 -0400

    Clean function and rudimentary bin classifier

---
+++ titanicBinClf.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Wed May 16 09:24:09 2018
+@author: nealbayya
+"""
+import pandas as pd
+import numpy as np
+from cleanTitanic import *
+
+def main():
+    [traindf, testdf, data_type_dict] = cleanTitanic.clean()
+
+
+
+
+if __name__ == '__main__':
+    main()
